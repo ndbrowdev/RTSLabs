ec1140609e300382dcb0e9cb5bef3d48be5c4397
ndbrowdev <98305757+ndbrowdev@users.noreply.github.com>
2022-01-24 02:04:31 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.Json;
+
+namespace RTSLabs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rts = new RTSLabs();
+            int[] numbers = { 1, 5, 2, 1, 10 };
+            var aboveBelowResult = rts.aboveBelow(numbers,6);
+
+            var result = JsonSerializer.Serialize(aboveBelowResult);
+            Console.WriteLine(result);
+
+            result = rts.stringRotation("MyString", 2);
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ RTSLabs.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace RTSLabs
+{
+    public class RTSLabs
+    {
+        public AboveBelowClass aboveBelow (int[] numbers, int comparisonValue)
+        {
+            var numBelow = 0;
+            var numAbove = 0;
+
+            for (var i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] < comparisonValue)
+                    numBelow++;
+                else if (numbers[i] > comparisonValue)
+                    numAbove++;
+            }
+
+            var result = new AboveBelowClass();
+            result.above = numAbove;
+            result.below = numBelow;
+
+            return result;
+        }
+
+        public string stringRotation (string str, int rotationAmount)
+        {
+            var arr = new char[str.Length];
+
+            for (var i = 0; i < str.Length; i++)
+            {
+                arr[(i + rotationAmount) % str.Length] = str[i];
+            }
+
+            return new string(arr);
+        }
+    }
+
+    public class AboveBelowClass
+    {
+        public int above { get; set; }
+        public int below { get; set; }
+    }
+}
